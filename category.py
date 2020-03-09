466626c4637961fa595c3b66977f5580e4f01060
the34show <alhung.hung@mail.utoronto.ca>
2020-03-09 09:45:21 -0400

    started opends2()

--- category.py
+++ category.py
@@ -42,6 +42,35 @@ def game():
             return ("Dead")
     
 
+def opends2():
+    #opens the ds
+    first_to_name={}
+    f=open("name.csv","r")
+    header=f.readline()
+    for line in f:
+        name=line.strip("\n")
+        first_char=name[0]
+        if first_char in first_to_name:
+            first_to_name[first_char].append(name)
+        else:
+            first_to_name[first_char]=[name]
+    f.close()
+    return first_to_name
 
+def categories():
+    alive=True
+    pokemon="1"
+    inital_letter=""
+    global first_to_name
+    first_to_name=opends2()
+    while alive== True:
+        inital_letter=letter(inital_letter)        
+        while pokemon[0]!= inital_letter:
+            pokemon= input("What pokemon stats with "+ inital_letter + "? ")
+            pokemon=pokemon.capitalize()
+        inital_letter=pokemon[-1]
+        inital_letter=inital_letter.capitalize()
+        alive=False
+        
 
 
